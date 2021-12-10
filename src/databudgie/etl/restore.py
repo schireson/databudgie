d89a015570a751e09c39bb4962cbf5d97571412f
Dan Cardin <ddcardin@gmail.com>
2021-12-10 16:20:27 -0500

    feat: Support local backups, make boto3 an optional dependency.

--- src/databudgie/etl/restore.py
+++ src/databudgie/etl/restore.py
@@ -1,10 +1,12 @@
 import contextlib
 import io
+import os
+import pathlib
+from dataclasses import dataclass
 from datetime import datetime
-from typing import Generator, Iterable, List, Optional, Tuple
+from typing import Generator, Iterable, List, Optional, Tuple, TYPE_CHECKING
 
 from configly import Config
-from mypy_boto3_s3.service_resource import Bucket, ObjectSummary, S3ServiceResource
 from setuplog import log
 from sqlalchemy.orm import Session
 
@@ -12,7 +14,11 @@ from databudgie.adapter import Adapter
 from databudgie.compat import TypedDict
 from databudgie.etl.base import expand_table_ops, TableOp
 from databudgie.manifest.manager import Manifest
-from databudgie.utils import capture_failures, FILENAME_FORMAT, parse_table, S3Location, wrap_buffer
+from databudgie.s3 import is_s3_path, optional_s3_resource, S3Location
+from databudgie.utils import capture_failures, FILENAME_FORMAT, parse_table, wrap_buffer
+
+if TYPE_CHECKING:
+    from mypy_boto3_s3.service_resource import Bucket, S3ServiceResource
 
 
 class RestoreConfig(TypedDict):
@@ -23,24 +29,31 @@ class RestoreConfig(TypedDict):
 
 def restore_all(
     session: Session,
-    s3_resource: S3ServiceResource,
     config: Config,
     manifest: Optional[Manifest] = None,
-    strict=False,
     adapter: Optional[str] = None,
+    strict=False,
 ) -> None:
     """Perform restore on all tables in the config."""
+    concrete_adapter = Adapter.get_adapter(adapter or session)
+    s3_resource = optional_s3_resource(config)
+
     table_ops = expand_table_ops(session, config.restore.tables, manifest=manifest)
 
-    actual_adapter = Adapter.get_adapter(adapter or session)
-
-    truncate_tables(session, table_ops, adapter=actual_adapter)
+    truncate_tables(session, table_ops, adapter=concrete_adapter)
 
     for table_op in table_ops:
         log.info(f"Restoring {table_op.table_name}...")
 
         with capture_failures(strict=strict):
-            restore(session, s3_resource, config=config, table_op=table_op, manifest=manifest, adapter=actual_adapter)
+            restore(
+                session,
+                config=config,
+                table_op=table_op,
+                manifest=manifest,
+                adapter=concrete_adapter,
+                s3_resource=s3_resource,
+            )
 
 
 def truncate_tables(session: Session, table_ops: List[TableOp], adapter: Adapter):
@@ -54,12 +67,12 @@ def truncate_tables(session: Session, table_ops: List[TableOp], adapter: Adapter
 
 def restore(
     session: Session,
-    s3_resource: S3ServiceResource,
     *,
     config: Config,
     adapter: Adapter,
     table_op: TableOp,
     manifest: Optional[Manifest] = None,
+    s3_resource: Optional["S3ServiceResource"] = None,
 ) -> None:
     """Restore a CSV file from S3 to the database."""
 
@@ -68,41 +81,74 @@ def restore(
     table_name = f"{schema}.{table}"
 
     strategy = table_op.raw_conf.get("strategy", "use_latest_filename")
-    with _download_from_s3(s3_resource, table_op.location(config), strategy) as (buffer, s3_path):
+    with get_file_contents(table_op.location(config), strategy, s3_resource=s3_resource) as (buffer, path):
         with wrap_buffer(buffer) as wrapper:
             with session:
                 adapter.import_csv(session, wrapper, table_op.table_name)
                 session.commit()
 
         if manifest:
-            manifest.record(table_name, s3_path)
+            manifest.record(table_name, path)
 
-    log.info(f"Restored {table_name} from {s3_path}")
+    log.info(f"Restored {table_name} from {path}")
+
+
+@dataclass(frozen=True)
+class ObjectSummary:
+    key: str
+    last_modified: datetime
+
+    @property
+    def path(self):
+        return pathlib.PurePath(self.key)
+
+    @classmethod
+    def from_stat(cls, name, stat: os.stat_result):
+        return cls(name, datetime.fromtimestamp(stat.st_mtime))
 
 
 @contextlib.contextmanager
-def _download_from_s3(
-    s3_resource: S3ServiceResource, location: str, strategy: str
+def get_file_contents(
+    location: str,
+    strategy: str,
+    s3_resource: Optional["S3ServiceResource"] = None,
 ) -> Generator[Tuple[io.BytesIO, str], None, None]:
-    # this location.key should be a folder
-    s3_location = S3Location(location)
-    s3_bucket: Bucket = s3_resource.Bucket(s3_location.bucket)
-
-    object_generator: Iterable[ObjectSummary] = s3_bucket.objects.filter(Prefix=s3_location.key)
-    target_object: ObjectSummary = VALID_STRATEGIES[strategy](object_generator)
-    log.info(f"Using {target_object.key}...")
+    concrete_strategy = VALID_STRATEGIES[strategy]
 
     buffer = io.BytesIO()
-    s3_bucket.download_fileobj(target_object.key, buffer)
+    if is_s3_path(location):
+        # this location.key should be a folder
+        s3_location = S3Location(location)
+        s3_bucket: "Bucket" = s3_resource.Bucket(s3_location.bucket)  # type: ignore
+
+        object_generator = (
+            ObjectSummary(o.key, o.last_modified) for o in s3_bucket.objects.filter(Prefix=s3_location.key)
+        )
+        target_object: ObjectSummary = concrete_strategy(object_generator)
+
+        s3_bucket.download_fileobj(target_object.key, buffer)
+        path = f"s3://{s3_location.bucket}/{target_object.key}"
+    else:
+        object_generator = (
+            ObjectSummary.from_stat(os.path.sep.join([location, dir_entry.name]), dir_entry.stat())
+            for dir_entry in os.scandir(location)
+            if dir_entry.is_file()
+        )
+        target_object = concrete_strategy(object_generator)
+
+        with open(target_object.key, "rb") as f:
+            buffer.write(f.read())
+        path = target_object.key
+
+    log.info(f"Using {target_object.key}...")
+
     buffer.seek(0)
-
-    yield buffer, f"s3://{s3_location.bucket}/{target_object.key}"
-
+    yield buffer, path
     buffer.close()
 
 
 def _use_filename_strategy(available_objects: Iterable[ObjectSummary]) -> ObjectSummary:
-    objects_by_filename = {s3_object.key.split("/")[-1]: s3_object for s3_object in available_objects}
+    objects_by_filename = {obj.path.name: obj for obj in available_objects}
     ordered_filenames = sorted(
         objects_by_filename.keys(), key=lambda x: datetime.strptime(x, FILENAME_FORMAT), reverse=True
     )
